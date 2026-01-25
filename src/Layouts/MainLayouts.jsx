4eb65e5164ff164ab3ee487e2a93733f0b2f1d4a
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-01-25 11:24:23 +0600

    resolve

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -5,7 +5,7 @@ import Footer from "../pages/Shared/Footer/Footer";
 
 const MainLayouts = () => {
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <Navbar></Navbar>
       <div className="max-w-6xl mx-auto">
         <Outlet></Outlet>
