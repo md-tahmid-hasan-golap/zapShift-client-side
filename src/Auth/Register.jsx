e76da6ce4e0bbaffb7544e28f7d04c500e7283ce
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-01-31 22:47:24 +0600

    add a firebase

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handelRegister = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <h2>This is Register</h2>
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center md:text-left">
+        Create an Account
+      </h2>
+      <form onSubmit={handleSubmit(handelRegister)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6 })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500 text-sm">
+              Password must be at least 6 characters
+            </p>
+          )}
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+
+      <p className="text-center font-medium pt-3">
+        Already Have An Account{" "}
+        <Link className="text-blue-500 underline " to="/login">
+          Login
+        </Link>
+      </p>
     </div>
   );
 };
---
+++ src/firebase/FirebaseAuthProvider.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { createContext } from "react";
+import { auth } from "./firebase.init";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const provider = new GoogleAuthProvider();
+
+const FirebaseAuthProvider = ({ children }) => {
+  // setUser
+  const [user, setUser] = useState(null);
+  // setLoading
+  const [loading, setLoading] = useState(true);
+  // creatUser
+  const creatUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // signIsUser
+  const signInUser = (email, password) => {
+    setLoading(true);
+
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // signInWithGoogle
+  const signInWithGoogle = () => {
+    setLoading(true);
+
+    return signInWithPopup(auth, provider);
+  };
+
+  // Observe User
+
+  useEffect(() => {
+    const unSuscribe = onAuthStateChanged(auth, (crueentUser) => {
+      setUser(crueentUser);
+      setLoading(false);
+
+      console.log(crueentUser);
+    });
+    return () => unSuscribe();
+  }, []);
+
+  // logOut
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  const userInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    creatUser,
+    signInUser,
+    signInWithGoogle,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default FirebaseAuthProvider;
