73db7a254392dd6e668e09acdb8b2f4312139609
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-03-01 22:17:56 +0600

    add a logout

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import toast from "react-hot-toast";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
+
   const handelLogout = () => {
     logOut()
       .then((result) => {
@@ -14,8 +15,10 @@ const Navbar = () => {
       })
       .catch((error) => {
         console.log(error);
+        toast.error("Logout failed!");
       });
   };
+
   const links = (
     <>
       <li>
@@ -23,8 +26,9 @@ const Navbar = () => {
       </li>
     </>
   );
+
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm px-2 md:px-4">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -35,76 +39,70 @@ const Navbar = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
-              {" "}
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
                 d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
+              />
             </svg>
           </div>
           <ul
             tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            {links}{" "}
+            {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">
-          <Logo></Logo>
-        </a>
+        <Link to="/" className="btn btn-ghost text-xl p-0 md:p-2">
+          <Logo />
+        </Link>
       </div>
+
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
+        <ul className="menu menu-horizontal px-1 font-medium">{links}</ul>
       </div>
-      {user ? (
-        <div className="navbar-end">
+
+      {/* Right Side Section */}
+      <div className="navbar-end gap-2 md:gap-3">
+        {/* Login ba Logout button (Left side of 'Be a rider') */}
+        {user ? (
           <button
             onClick={handelLogout}
-            className="
-                btn btn-sm md:btn-md 
-                bg-red-500 hover:bg-red-600 
-                border-none text-white 
-                normal-case px-4 md:px-6
-              "
+            className="btn btn-sm md:btn-md bg-red-500 hover:bg-red-600 border-none text-white normal-case px-3 md:px-5 rounded-lg"
           >
             Logout
           </button>
-        </div>
-      ) : (
-        <div className="navbar-end gap-2">
-          <Link to="/login" className="btn btn-outline">
+        ) : (
+          <Link
+            to="/login"
+            className="btn btn-sm md:btn-md btn-outline border-gray-300 normal-case"
+          >
             Sign In
           </Link>
-          <Link
-            to="/beARider"
-            className="
-    /* Background and Text Color */
-    bg-[#D9F99D] 
-    hover:bg-[#bef264] 
-    text-black 
-    
-    /* Typography */
-    font-semibold 
-    normal-case 
-    
-    /* Padding & Shape */
-    px-6 
-    py-2 
-    rounded-xl 
-    
-    /* Layout & Transition */
-    btn 
-    border-none 
-    transition-all 
-    duration-300
-  "
-          >
-            Be a rider
-          </Link>
-        </div>
-      )}
+        )}
+
+        {/* 'Be a rider' - Ekebare Right Side-e thakbe */}
+        <Link
+          to="/beARider"
+          className="
+            btn btn-sm md:btn-md
+            bg-[#D9F99D] 
+            hover:bg-[#bef264] 
+            text-black 
+            font-bold 
+            normal-case 
+            px-3 md:px-6 
+            rounded-xl 
+            border-none 
+            transition-all 
+            duration-300
+            shadow-sm
+          "
+        >
+          Be a rider
+        </Link>
+      </div>
     </div>
   );
 };
